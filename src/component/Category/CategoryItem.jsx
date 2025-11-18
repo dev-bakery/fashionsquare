6430dd65b69665a91b5d19a4c5448256a4a33988
dev-bakery <rimming2255@naver.com>
2025-11-18 16:04:30 +0900

    카테고리 레이어 닫기 onSelect 추가

--- src/component/Category/CategoryItem.jsx
+++ src/component/Category/CategoryItem.jsx
@@ -3,12 +3,12 @@ import React from "react";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 
-const CategoryItem = ({ index, category }) => {
+const CategoryItem = ({ index, category, onSelect }) => {
   const pathname = usePathname();
   const { categoryImageUrl, categoryName, categoryNo } = category || {};
   return (
     <li className='list-item'>
-      <Link className='link__category' href={`${pathname}?f=c:${categoryNo}`}>
+      <Link className='link__category' href={`${pathname}?f=c:${categoryNo}`} onClick={onSelect}>
         <span className='box__category-img'>
           <img src={categoryImageUrl} alt='' className='image' />
         </span>
--- src/component/Category/CategoryLayer.jsx
+++ src/component/Category/CategoryLayer.jsx
@@ -7,32 +7,17 @@ import { categoryLayerAtom } from "../atom/atom";
 const CategoryLayer = ({ data }) => {
   const [categoryLayer, setCategoryLayer] = useAtom(categoryLayerAtom);
   return (
-    <div
-      className={classNames(
-        "box__layer-category",
-        categoryLayer && "box__layer-category--active"
-      )}
-      role='dialog'
-      aria-modal='true'
-    >
+    <div className={classNames("box__layer-category", categoryLayer && "box__layer-category--active")} role='dialog' aria-modal='true'>
       <div className='box__layer-content'>
         <div className='box__layer-title'>
           <p className='text__title'>패션스퀘어 카테고리</p>
-          <button
-            type='button'
-            className='button__close'
-            onClick={() => setCategoryLayer((prev) => !prev)}
-          >
+          <button type='button' className='button__close' onClick={() => setCategoryLayer((prev) => !prev)}>
             <span className='for-a11y'>레이어 닫기</span>
           </button>
         </div>
         <ul className='list__category'>
           {data?.map((category, index) => (
-            <CategoryItem
-              key={category.categoryNo}
-              index={index}
-              category={category}
-            />
+            <CategoryItem key={category.categoryNo} index={index} category={category} onSelect={() => setCategoryLayer(false)} />
           ))}
         </ul>
       </div>
